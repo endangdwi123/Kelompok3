82836898c8818f8d61d545fb65b021251249a634
palcomtech <dwikwira9@gmail.com>
2023-05-10 09:53:21 +0700

    tugas

--- lib/militer.dart
+++ lib/militer.dart
@@ -12,8 +12,25 @@ class Militer extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text('Anggota Komisi III DPR Curhat Berita Politik dan Koalisi Partai Tenggelam karena Kasus Ferdy Sambo '),
+          children: [
+            Image.asset(
+              'assets/img/dunia.jpg',
+              height: 200,
+            ),
+            SizedBox(
+              height: 5.0,
+              ),
+            Text('Kekuatan Militer Indonesia Peringkat 16 Besar',),
+            const SizedBox(
+              height: 20,
+            ),
+            Text('Situs Global Fire Power (GFP) merilis indeks kekuatan militer negara-negara di dunia pada 2021. Melalui rilis itu, posisi Indonesia ini berada di atas peringkat negara-negara seperti Spanyol, Australia, dan Israel yang masing-masing bertengger di posisi 18, 19, dan 20. Adapun peringkat pertama ditempati oleh Amerika Serikat (AS). Selanjutnya disusul oleh Rusia di peringkat kedua dan China di peringkat ketiga. Kemudian di posisi keempat dan kelima masing-masing ditempati oleh India dan Jepang. Dalam hal ini, GFP tak memperhitungkan perbandingan kualitas alutsista tiap negara. Indeks GFP hanya menitikberatkan pada aspek kuantitas dan tidak mengukur atau melihat kesiapan tempur angkatan perang sebuah negara. Sehingga, meski Indonesia di peringkat 16, hal ini dinilai tidak membuat bangga secara berlebihan.')
+            ,SizedBox(
+              height: 20,
+            ),
+            const CircularProgressIndicator.adaptive(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
           ],
         ),
       ),
--- lib/politik.dart
+++ lib/politik.dart
@@ -12,8 +12,25 @@ class Politik extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text('Saya Satpam Masuk Hari Libur Nasiona, Apakah Berhak Uang Lembur?'),
+          children: [
+            Image.asset(
+              'assets/img/wihadi.png',
+              height: 200,
+            ),
+            SizedBox(
+              height: 5.0,
+              ),
+            Text('Anggota Komisi III DPR Curhat Berita Politik dan Koalisi Partai Tenggelam karena Kasus Ferdy Sambo '),
+            const SizedBox(
+              height: 20,
+            ),
+            Text('Hal itu yang disampaikan Wihadi Wiyanto Anggota Komisi III DPR dari Fraksi Partai Gerindra dalam rapat bersama Kapolri di Gedung Parlemen, Senayan, Rabu (24/8/2022). Artikel ini telah tayang di Tribunnews.com dengan judul Anggota Komisi III DPR Curhat Berita Politik dan Koalisi Partai Tenggelam karena Kasus Ferdy Sambo')
+            ,SizedBox(
+              height: 20,
+            ),
+            const CircularProgressIndicator.adaptive(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
           ],
         ),
       ),
